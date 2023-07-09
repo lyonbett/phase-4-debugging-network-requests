3067142b776d993238b0864405c31c528a75b177
lyonbett <lyon.bett@student.moringaschool.com>
2023-07-10 01:57:54 +0300

    completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,19 @@
 class MoviesController < ApplicationController
-  
+
   def index
     movies = Movie.all
     render json: movies
+  end
+
+  def create
+    movie = Movie.create(movies_params)
+    render json: movie, status: :created
+  end
+
+  private
+
+  def movies_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
   end
 
 end
